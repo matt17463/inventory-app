5a583334a8ef9742f4397737337114b9b5fff1ab
matt17463 <96403201+matt17463@users.noreply.github.com>
2026-05-28 12:44:16 -0700

    Refactor App component to include Header and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Header from './Header';
+
 import CreateProduct from './CreateProduct';
 import AssignBin from './AssignBin';
 import BinContents from './BinContents';
@@ -9,40 +11,23 @@ import TestTag from './TestTag';
 import Home from './Home';
 import SelectProduct from './SelectProduct';
 
-
-
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
+            <Header />
 
-                <Route path="/" element={<Home />} />
-
-
-                {/* Bins dashboard */}
-                <Route path="/bins" element={<BinsDashboard />} />
-
-                <Route path="/select-product" element={<SelectProduct />} />
-                <Route path="/create-product" element={<CreateProduct />} />
-
-
-
-                {/* View bin contents */}
-                <Route path="/bin/:binId" element={<BinContents />} />
-
-                {/* Assign a product to a bin */}
-                <Route path="/assign-bin" element={<AssignBin />} />
-
-                {/* NFC tag writer */}
-                <Route path="/nfc-writer" element={<NfcWriter />} />
-
-                {/* NFC tag tester */}
-                <Route path="/test-tag" element={<TestTag />} />
-
-                {/* Create product (home) */}
-                <Route path="/" element={<CreateProduct />} />
-
-            </Routes>
+            <div style={{ paddingTop: "20px" }}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/bins" element={<BinsDashboard />} />
+                    <Route path="/select-product" element={<SelectProduct />} />
+                    <Route path="/create-product" element={<CreateProduct />} />
+                    <Route path="/bin/:binId" element={<BinContents />} />
+                    <Route path="/assign-bin" element={<AssignBin />} />
+                    <Route path="/nfc-writer" element={<NfcWriter />} />
+                    <Route path="/test-tag" element={<TestTag />} />
+                </Routes>
+            </div>
         </BrowserRouter>
     );
 }
