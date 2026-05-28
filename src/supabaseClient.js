98463d7be0d98ebba5ec55b27fdc3447b61b0fc0
matt17463 <96403201+matt17463@users.noreply.github.com>
2026-05-28 12:10:46 -0700

    test

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+console.log("ENV CHECK:", {
+  url: import.meta.env.VITE_SUPABASE_URL,
+  anon: import.meta.env.VITE_SUPABASE_ANON_KEY
+});
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
