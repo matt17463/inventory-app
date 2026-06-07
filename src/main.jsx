3bb98265bb8667f7d8d352e45beb811be0aa0fbd
matt17463 <96403201+matt17463@users.noreply.github.com>
2026-06-06 21:55:14 -0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles.css';
+import App from './App.jsx';
+import './App.css';
+import { ThemeProvider } from './ui/ThemeProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
